fee6dc9adc79dd2fec9f262efcf9cdd4e8683880
HamzaAhmedSheikh <hamzaahmedsheikh313@gmail.com>
2020-06-30 00:41:23 +0500

    Symptom section #2 complete with svg image

--- src/components/Symptom.js
+++ src/components/Symptom.js
@@ -6,6 +6,10 @@ import Tab from '@material-ui/core/Tab';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
 
+import Doctor from "../img/doctor.svg";
+
+
+
 function TabPanel(props) {
   const { children, value, index, ...other } = props;
 
@@ -38,6 +42,7 @@ const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
     backgroundColor: theme.palette.background.paper,
+    margin: 20,
   },
 }));
 
@@ -54,7 +59,7 @@ export default function SimpleTabs() {
       <AppBar position="static">
         <Tabs value={value} onChange={handleChange} aria-label="simple tabs example">
           <Tab label="Overview" {...a11yProps(0)} />
-          <Tab label="Item Two" {...a11yProps(1)} />
+          <Tab label="Symptoms" {...a11yProps(1)} />
           <Tab label="Item Three" {...a11yProps(2)} />
           <Tab label="Item Four" {...a11yProps(3)} />
         </Tabs>
@@ -66,16 +71,18 @@ export default function SimpleTabs() {
       <hr /> 
 
       <p> HOW IT SPREADS </p>
-      <p>The virus that causes COVID-19 is mainly transmitted through droplets generated
-         when an infected person coughs, sneezes, or exhales.
-         These droplets are too heavy to hang in the air, and quickly fall on floors or surfaces.
+      <p>
+        The virus that causes COVID-19 is mainly transmitted through droplets generated
+        when an infected person coughs, sneezes, or exhales.
+        These droplets are too heavy to hang in the air, and quickly fall on floors or surfaces.
      </p>
-     <p>You can be infected by breathing in the virus if you are within close proximity
-        of someone who has COVID-19, or by touching a contaminated surface and
-        then your eyes, nose or mouth.  
+     <p>
+       You can be infected by breathing in the virus if you are within close proximity
+       of someone who has COVID-19, or by touching a contaminated surface and
+       then your eyes, nose or mouth.  
      </p>
 
-     <p> Source: 
+     <p> Source:{" "}
         <a href="https://www.who.int/emergencies/diseases/novel-coronavirus-2019"
           rel="noopener noreferrer"
           target="_blank">
@@ -87,7 +94,64 @@ export default function SimpleTabs() {
 
       </TabPanel>
       <TabPanel value={value} index={1}>
-        Item Two
+        <p>
+          COVID-19 affects different people in different ways.
+          Most infected people will develop mild to moderate illness and recover without hospitalization.
+        </p>
+        <hr />
+
+        <p>Most common symptoms:</p>
+        <ul>
+          <li>fever</li>
+          <li>dry cough</li>
+          <li>tiredness</li>
+        </ul>
+
+        <p>Less common symptoms:</p>
+        <ul>
+          <li>aches and pains</li>
+          <li>sore throat</li>
+          <li>diarrhoea</li>
+          <li>conjunctivitis</li>
+          <li>headache</li>
+          <li>loss of taste or smell</li>
+          <li>a rash on skin, or discolouration of fingers or toes</li>
+        </ul>
+
+        <p>Serious symptoms:</p>
+        <ul>
+          <li>difficulty breathing or shortness of breath</li>
+          <li>chest pain or pressure</li>
+          <li>loss of speech or movement</li>
+        </ul>
+
+        <p>
+          Seek immediate medical attention if you have serious symptoms.
+          Always call before visiting your doctor or health facility.
+        </p>
+
+        <img src={Doctor} style={{ height: 350 }} alt="doctor"/>
+
+        <p>
+          People with mild symptoms who are otherwise healthy should manage
+          their symptoms at home.
+        </p>
+
+        <p>
+          On average it takes 5–6 days from when someone is infected with the
+          virus for symptoms to show, however it can take up to 14 days.
+        </p>
+
+        <p>
+          Source:{" "}
+          <a href="https://www.who.int/emergencies/diseases/novel-coronavirus-2019/question-and-answers-hub/q-a-detail/q-a-coronaviruses#:~:text=symptoms"
+             rel="noopener noreferrer"
+             target="_blank"          
+          >
+              who.int
+          </a>
+        </p>
+
       </TabPanel>
       <TabPanel value={value} index={2}>
         Item Three

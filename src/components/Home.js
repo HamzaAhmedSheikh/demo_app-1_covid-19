e2edcb30ab56567dfa8aebded829b7b5e364893f
HamzaAhmedSheikh <hamzaahmedsheikh313@gmail.com>
2020-07-01 05:14:34 +0500

    add search input

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,20 +1,21 @@
 import React, { useState, useEffect } from 'react'
 import axios from "axios"
-import { Card, CardContent, Typography, Grid, CardMedia } from '@material-ui/core'
+import { Card, CardContent, Typography, Grid, CardMedia, TextField } from '@material-ui/core'
 import NumberFormat from 'react-number-format';
 
 import "../css/Home.css"
 import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles({
-    root: {
-       maxWidth: 500,            
+    root: {                   
       textAlign: "center",
-      margin: 30,            
+      margin: 20,              
       display: "inline-block",       
     },
     img: {
-        height: 200,        
+        height: 200, 
+        width: 365,
+        padding: 12,       
     }
 
   }); 
@@ -24,6 +25,7 @@ function Home() {
 
     const [latest, setLatest] = useState([])
     const [results, setResults] = useState([])
+    const [searchCountry, setSearchCountry] = useState("")
 
     useEffect(() => {
         axios.all([
@@ -43,7 +45,14 @@ function Home() {
     const date = new Date(parseInt(latest.updated));
     const lastUpdated = date.toString()
     let classes = useStyles();
-    const countries = results.map((data, i) => {
+
+
+
+    const filterCountry = results.filter(item => {
+        return searchCountry !== "" ? item.country === searchCountry : item
+    })
+
+    const countries = filterCountry.map((data, i) => {
        
         return (
 
@@ -84,25 +93,7 @@ function Home() {
         </CardContent>
       
     </Card>
-    //      <div className="container" key={i}>             
-    //     <Grid container spacing={3} justify="center" >
-    //      <Grid item  xs={12} md={3}>
-    //       <CardContent>
-    //        <Typography color="textSecondary" gutterBottom> <img src={data.countryInfo.flag} alt="countries"/>  </Typography>
-    //         <Typography variant="h5">   
-    //           <h5> {data.country} </h5>            
-    //           <h4> Cases {data.cases} </h4>        
-    //           <h4> Deaths {data.deaths} </h4>  
-    //           <h4> Recovered {data.recovered }</h4> 
-    //           <h4> Today's cases {data.todayCases} </h4>
-    //           <h4> Today's deaths {data.todayDeaths} </h4>
-    //           <h4> Active {data.active}</h4>
-    //           <h4> Critical {data.critical}</h4>           
-    //         </Typography>
-    //       </CardContent>
-    //      </Grid>
-    //     </Grid>
-    // </div>
+    
    )
  })
 
@@ -110,50 +101,58 @@ function Home() {
 
     return (
         <div className="container">
-            <Grid container spacing={3} justify="center">
-                <Grid item component={Card} xs={12} md={3} className="card">
-                    <CardContent>
-                        <Typography color="textSecondary" gutterBottom>  Infected  </Typography>
-                        <Typography variant="h5">
+         <Grid container spacing={3} justify="center">
+          <Grid item component={Card} xs={12} md={3} className="card">
+           <CardContent>
+             <Typography color="textSecondary" gutterBottom>  Infected  </Typography>
+              <Typography variant="h5">
+                <NumberFormat value={latest.cases}
+                              displayType={'text'}
+                              thousandSeparator={true} />
+              </Typography>
+              <Typography color="textSecondary">
+                   {new Date(lastUpdated).toDateString()} 
+              </Typography>
+              <Typography variant="body2"> Number of active cases of COVID-19 </Typography>
+           </CardContent>
+         </Grid>
 
-                            <NumberFormat value={latest.cases} displayType={'text'} thousandSeparator={true} />
+          <Grid item component={Card} xs={12} md={3} className="card1">
+            <CardContent>
+              <Typography color="textSecondary" gutterBottom>  Recovered  </Typography>
+              <Typography variant="h5">
+                <NumberFormat value={latest.recovered}
+                              displayType={'text'}
+                              thousandSeparator={true} />
+              </Typography>
+              <Typography color="textSecondary"> {new Date(lastUpdated).toDateString()} </Typography>
+              <Typography variant="body2"> Number of recoveries from COVID-19 </Typography>
+            </CardContent>
+          </Grid>
 
-                        </Typography>
-                        <Typography color="textSecondary"> {new Date(lastUpdated).toDateString()}   </Typography>
-                        <Typography variant="body2"> Number of active cases of COVID-19 </Typography>
-                    </CardContent>
-                </Grid>
-
-                <Grid item component={Card} xs={12} md={3} className="card1">
-                    <CardContent>
-                        <Typography color="textSecondary" gutterBottom>  Recovered  </Typography>
-                        <Typography variant="h5">
-                            <NumberFormat value={latest.recovered} displayType={'text'} thousandSeparator={true} />
-
-
-
-                        </Typography>
-                        <Typography color="textSecondary"> {new Date(lastUpdated).toDateString()} </Typography>
-                        <Typography variant="body2"> Number of recoveries from COVID-19 </Typography>
-                    </CardContent>
-                </Grid>
-
-                <Grid item component={Card} xs={12} md={3} className="card2">
-                    <CardContent>
-                        <Typography color="textSecondary" gutterBottom>  Deaths  </Typography>
-                        <Typography variant="h5">
-                            <NumberFormat value={latest.deaths} displayType={'text'} thousandSeparator={true} />
-
-
-
-                        </Typography>
-                        <Typography color="textSecondary"> {new Date(lastUpdated).toDateString()} </Typography>
-                        <Typography variant="body2"> Number of death caused by COVID-19 </Typography>
-                    </CardContent>
-                </Grid>
+            <Grid item component={Card} xs={12} md={3} className="card2">
+             <CardContent>
+               <Typography color="textSecondary" gutterBottom>  Deaths  </Typography>
+               <Typography variant="h5">
+                 <NumberFormat value={latest.deaths}
+                               displayType={'text'}
+                               thousandSeparator={true} />
+               </Typography>
+                <Typography color="textSecondary"> {new Date(lastUpdated).toDateString()} </Typography>
+                <Typography variant="body2"> Number of death caused by COVID-19 </Typography>
+              </CardContent>
             </Grid>
 
-              {countries}
+
+            <form className={classes.root} noValidate autoComplete="on">
+               <TextField id="outlined-basic"
+                        label="Search Your Country" 
+                        variant="outlined"
+                        onChange={e => setSearchCountry(e.target.value)} />
+            </form>
+          </Grid>
+
+             {countries}  
                
         </div>
 

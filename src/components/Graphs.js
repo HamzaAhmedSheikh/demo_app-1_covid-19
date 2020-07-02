55f57af61053a1540a2578a909f943fc98d6b283
HamzaAhmedSheikh <hamzaahmedsheikh313@gmail.com>
2020-07-03 00:30:45 +0500

    add chart

--- src/components/Graphs.js
+++ src/components/Graphs.js
@@ -1,6 +1,25 @@
 import React, { useState, useEffect } from 'react'
-import { Line } from 'react-chartjs-2';
-import axios from "axios"
+import { Line } from "react-chartjs-2";
+
+const data = {
+    labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul"],    
+    datasets: [
+      {
+        label: "First dataset",
+        data: [33, 53, 85, 41, 44, 65, 100],
+        fill: true,
+        backgroundColor: "rgba(75,192,192,0.2)",
+        borderColor: "rgba(75,192,192,1)"
+      },
+      {
+        label: "Second dataset",
+        data: [33, 25, 35, 51, 54, 76, 86],
+        fill: false,
+        borderColor: "#742774"
+      }
+    ]
+  };
+  
 
 function Graph () {
 
@@ -8,7 +27,9 @@ function Graph () {
 
 
     return(
-        <strong> HELLO WORLD   </strong> 
+        <div>
+          <Line data={data} />
+        </div>
     )
 }
 

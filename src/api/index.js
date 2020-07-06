fc87536c94c2a62022ca94e1887df35e8ed2f04a
HamzaAhmedSheikh <hamzaahmedsheikh313@gmail.com>
2020-07-07 02:18:42 +0500

    add three cards

--- src/api/index.js
+++ src/api/index.js
@@ -4,14 +4,14 @@ const url = "https://covid19.mathdro.id/api"
 
  export const fetchData = async (country) => {
 
-   let changeableUrl = url 
+//    let changeableUrl = url 
 
-     if(country) {
-        changeableUrl = `${url}/countries/${country}`
-     }
+//      if(country) {
+//         changeableUrl = `${url}/countries/${country}`
+//      }
 
     try{
-        const { data: { confirmed, recovered, deaths, lastUpdate} } = await axios.get(changeableUrl)
+         const { data: { confirmed, recovered, deaths, lastUpdate} } = await axios.get(url)
 
          return { confirmed, recovered, deaths, lastUpdate }
 
@@ -24,34 +24,34 @@ const url = "https://covid19.mathdro.id/api"
     }
 }
 
- export const fetchDailyData = async () => {
-     try {
-        const { data } = await axios.get(`${url}/daily`)  
+//  export const fetchDailyData = async () => {
+//      try {
+//         const { data } = await axios.get(`${url}/daily`)  
         
-        const modifiedData = data.map((dailyData) => ({
-            confirmed: dailyData.confirmed.total,
-            deaths: dailyData.deaths.total,
-            date: dailyData.reportDate,
-        }))
+//         const modifiedData = data.map((dailyData) => ({
+//             confirmed: dailyData.confirmed.total,
+//             deaths: dailyData.deaths.total,
+//             date: dailyData.reportDate,
+//         }))
 
-          return modifiedData
+//           return modifiedData
           
 
-     } catch (error) {
+//      } catch (error) {
          
-        console.log(error)        
-     }
- }
+//         console.log(error)        
+//      }
+//  }
 
- export const fetchCountries = async () => {
-     try {
-         const { data: { countries } } = await axios.get(`${url}/countries`)       
+//  export const fetchCountries = async () => {
+//      try {
+//          const { data: { countries } } = await axios.get(`${url}/countries`)       
          
-         return countries.map((country) => country.name)
+//          return countries.map((country) => country.name)
          
-    } catch (error) {
+//     } catch (error) {
 
-        console.log(error);                 
-     }
- }
+//         console.log(error);                 
+//      }
+//  }
  
--- src/components/Graphs.js
+++ src/components/Graphs.js
@@ -1,21 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Cards from '../components1/Cards/Cards'
 import Chart from '../components1/Charts/Chart'
 import CountryPicker from '../components1/CountryPicker/CountryPicker'
+
+import { fetchData } from '../api'
 
 import styles from '../App.module.css'
 
 
 
-function Graph () {   
+class Graph extends Component {
+
+  state = {
+    data: {},    
+  }
+
+  async componentDidMount(){
+    const fetchedData = await fetchData()
+
+    this.setState({ data: fetchedData })
+       
+  }
+
+
+  render(){
+
+   const { data } = this.state
 
     return (
-        <div className={styles.container}>
-           <Cards />    
-           <CountryPicker />
-           <Chart />       
-        </div>
-    )
+      <div className={styles.container}>
+         <Cards data={ data } />    
+         <CountryPicker />
+         <Chart />       
+      </div>
+  )
+  }  
 }
 
 export default Graph 
